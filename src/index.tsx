cd9553e84bd08a5be17f2be6be5f816bc543bc07
Kate1892 <78145909+Kate1892@users.noreply.github.com>
2022-10-12 15:09:35 +0700

    adding HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.scss'
 import App from './App'
 
@@ -13,10 +13,10 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement)
 
   root.render(
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
